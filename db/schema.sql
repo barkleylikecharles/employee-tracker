6f3224ee649d8c28e246e8ef8e172ab2999620b4
Nicole Barkley <barkleylikecharles@icloud.com>
2022-05-07 13:11:17 -0400

    create tables working for dept, rol and employee

--- db/schema.sql
+++ db/schema.sql
@@ -2,20 +2,24 @@
 
 CREATE TABLE department (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
+  department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(20, 2) NULL,
-  department_id FOREIGN KEY (department_id)
+  dept_id INTEGER,
+  industry_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_dept FOREIGN KEY (dept_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id FOREIGN KEY (role_id);
---   manager_id FOREIGN KEY (department_id)
+  role_id INTEGER,
+  industry_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
 );
+--   manager_id FOREIGN KEY (department_id)
