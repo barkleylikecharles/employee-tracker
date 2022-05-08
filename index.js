66ef04a5372fd2c905a99e0d9e3cbc4e17977e6f
Nicole Barkley <barkleylikecharles@icloud.com>
2022-05-08 00:12:37 -0400

    updated dependencies

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const mysql = require('mysql2');
-
 const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+
+// Connection to employee_db Database
+const connection = mysql.createConnection ({
+    host: 'localhost',
+    port: 3001,
+    user: 'root',
+    password: '',
+    database: "employee_DB"
+})
