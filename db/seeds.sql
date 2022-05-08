ab209360cb7c4764d17f3f88752c3461480d8547
Nicole Barkley <barkleylikecharles@icloud.com>
2022-05-07 23:26:59 -0400

    add seeds for testing

---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO department (name)
+VALUES ("sales"),
+    ("finance"),
+    ("legal"),
+    ("engineering");
+
+SELECT * FROM DEPARTMENT;
+
+INSERT INTO role (title, salary, department_id),
+VALUES ("software engineer", 110000, 4),
+    ("accounting manager", 100000, 2),
+    ("engineering manager", 200000, 4),
+    ("accountant", 75000, 2),
+    ("sales manager", 120000, 1),
+    ("salesperson", 50000, 1),
+    ("legal team lead", 210000, 3),
+    ("lawyer", 150000, 3);
+
+SELECT * FROM role
+
+INSERT INTO
