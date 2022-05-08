ad4ca710724849ebfe5d4da0b81bd5fb45fe7cf7
Nicole Barkley <barkleylikecharles@icloud.com>
2022-05-07 23:56:28 -0400

    create and use database and file rename to index.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,5 @@
-
+CREATE DATABASE employee_DB;
+use employee_DB;
 
 CREATE TABLE department (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,12 @@
+use employee_DB;
+
 INSERT INTO department (name)
 VALUES ("sales"),
     ("finance"),
     ("legal"),
     ("engineering");
 
-SELECT * FROM DEPARTMENT;
+SELECT * FROM department;
 
 INSERT INTO role (title, salary, department_id),
 VALUES ("software engineer", 110000, 4),
@@ -18,4 +20,11 @@ VALUES ("software engineer", 110000, 4),
 
 SELECT * FROM role
 
-INSERT INTO
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Verona", "Nelson", 4, 2),
+     ("Bryan", "Daines", 7, NULL),
+     ("Nancy", "Birdd", 1, 4),
+     ("Peter", "Allen", 8, 3),
+     ("Henley", "Nelson", 5, NULL);
+
+SELECT * FROM employee;
