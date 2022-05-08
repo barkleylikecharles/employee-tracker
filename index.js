935031410d203beb1758b4cca114d1664e7951c5
Nicole Barkley <barkleylikecharles@icloud.com>
2022-05-08 00:28:51 -0400

    begin inquirer questions

--- index.js
+++ index.js
@@ -5,8 +5,34 @@ const consoleTable = require('console.table');
 // Connection to employee_db Database
 const connection = mysql.createConnection ({
     host: 'localhost',
-    port: 3001,
+    // port: 3000,
     user: 'root',
     password: '',
     database: "employee_DB"
 })
+
+connection.connect(function(err){
+    if (err) throw err;
+    mainMenu();
+})
+
+function mainMenu() {
+    inquirer
+        .prompt({
+            type: 'list',
+            message: 'Welcome to the employee database. How would you like to start?',
+            choices: [
+                'View All Employees',
+                'View All Departments',
+                "View All Roles",
+                "Add Employee",
+                "Add Department",
+                "Add Role",
+                // "Update Role",
+                // "Delete Employee",
+                "EXIT"
+            ]
+                
+    
+        })
+}
